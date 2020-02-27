0868da4728f624d350e244f6f0e3676b22c312b0
Ryan DeVree <54510931+ryan-devree-GRRMCO@users.noreply.github.com>
2020-02-26 20:06:26 -0500

    Update app-routing.module.ts

--- tmntPizza/src/app/app-routing.module.ts
+++ tmntPizza/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { MenuDetailComponent } from './menu-detail/menu-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'menu',
+    component: MenuComponent
+  },
+  {
+    path: 'menuDetail',
+    component: MenuDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

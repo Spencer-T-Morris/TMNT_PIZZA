a9f6960669ee9216d57d9aea16f1d1450fa86823
ben <benjamindrolls@gmail.com>
2020-02-26 19:53:15 -0500

    added more server functionality

--- tmntPizza/server.js
+++ tmntPizza/server.js
@@ -12,6 +12,7 @@ app.listen(port, () => {
 
 let menu = [
     {
+        id:0,
         name: "Beneath These Streets",
         price: 9,
         topping: "granola, and licorice",
@@ -21,5 +22,25 @@ let menu = [
 
 app.get('/pizza', (req, res)=> {
     res.json(menu)
+})
+app.get( '/pizza/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+
+    menu.map((item) => {
+
+        if (item.id === id) {
+            res.status(200).json(menu);
+        }
+    })
+});
+
+app.put('/pizza/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const body = req.body.data.quantity;
+
+    const update = menu.find(item => item.id === id)
+
+    update.quantity = body;
+    res.json("updated");
 })
 

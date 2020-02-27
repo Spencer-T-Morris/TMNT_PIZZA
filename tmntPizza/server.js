41772b67c1fd3fac88cc2e785bc89a50716cf309
ben <benjamindrolls@gmail.com>
2020-02-26 19:08:33 -0500

    server added

---
+++ tmntPizza/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`listing on http://localhost:${port}`)
+});
+
+let menu = [
+    {
+        name: "Beneath These Streets",
+        price: 9,
+        topping: "granola, and licorice"
+
+    }
+]
+
+app.get('/pizza', (req, res)=> {
+    res.json(menu)
+})
+

ed6e54bfa51f839b6b8c69cd95fe738057da099f
robertharman2 <robertharman2@yahoo.com>
2020-02-26 20:03:08 -0500

    Added menuitems to the array

--- tmntPizza/server.js
+++ tmntPizza/server.js
@@ -16,8 +16,57 @@ let menu = [
         price: 9,
         topping: "granola, and licorice",
         quantity: 0
+    },
+    {
+        id: 1,
+        name: "Attack of the 50-Foot Irma",
+        price: 10,
+        topping: "Chocolate Fudge, Extra Garlic",
+        quantity: 0
+    },
+    {
+        id: 2,
+        name: "April Fool",
+        price: 8,
+        topping: "Marshmallow, Pepperoni",
+        quantity: 0
+    },
+    {
+        id: 3,
+        name: "Green with Jealousy",
+        price: 9,
+        topping: "Peanut Butter, Clams",
+        quantity: 0
+    },
+    {
+        id: 4,
+        name: "Pizza by the Shred",
+        price: 8,
+        topping: "Peanut Butter, Avacodo, Extra Pickles",
+        quantity: 0
+    },
+    {
+        id: 5,
+        name: "Super Bebop and Mighty Rocksteady",
+        price: 9,
+        topping: "Marshmallow, Asparagus",
+        quantity: 0
+
+    },
+    {
+        id: 6,
+        name: "Case of the Hot Kimono",
+        price: 10,
+        topping: "Butterscotch, Onions, Anchovies",
+        quantity: 0
+    },
+    {
+        id: 7,
+        name: "The Making of Metalhead",
+        price: 10,
+        topping: "Tea, Toast"
     }
-]
+];
 
 app.get('/pizza', (req, res)=> {
     res.json(menu)
